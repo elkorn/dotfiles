b03e74e1a3c6c8947afffa3862f964c189dfb010
Korneliusz Caputa <helluinster@gmail.com>
2014-07-08 20:23:40 +0200

    Fixing xmobar/trayer config.

--- dotfiles/trayer.sh
+++ dotfiles/trayer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-trayer  --transparent true --alpha 1 --tint 0x000000 --edge top --align right --widthtype fixed --width 130 --heighttype fixed --height 10 --SetPartialStrut false --SetDockType true
+trayer  --transparent true --alpha 1 --tint 0x000000 --edge top --align right --widthtype percent --width 5 --heighttype fixed --height 10 --SetPartialStrut false --SetDockType true
 
