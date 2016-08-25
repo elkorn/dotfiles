b81f4964bdcd339b60cb83ed312a50d5683e3979
elkorn <helluinster@gmail.com>
2016-08-26 00:21:45 +0200

    Remove Emacs stuff.

--- .emacs.d/keybindings-global.el
+++
@@ -1,8 +0,0 @@
-(evil-leader/set-key "av" 'eval-buffer)
-(evil-leader/set-key "x" 'smex)
-(evil-leader/set-key "o" 'previous-buffer)
-(evil-leader/set-key "p" 'next-buffer)
-; (define-key evil-insert-state-map "jk" 'evil-normal-state)
-(evil-leader/set-key "l" 'projectile-find-file)
-(evil-leader/set-key "w" 'save-buffer)
-(evil-leader/set-key "i" 'list-buffers)
--- .emacs.d/keybindings-javascript.el
+++
@@ -1,1 +0,0 @@
-(evil-leader/set-key-for-mode 'js2-minor-mode "b" 'web-beautify-js)
